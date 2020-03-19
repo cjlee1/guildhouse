1d8a0ef316fd16d0bb3027c64f4b2f5328b5702c
cjlee1 <cjlee@ualberta.ca>
2020-03-18 18:06:44 -0600

    added images and buttons on main page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import styled from "styled-components";
-// import logo from '../svg/logo.svg'; 
+
+import logo from '../images/Logo.png'; 
 class Header extends Component {
   render() {
     return (
         <HeaderComponent className="header-container"> 
            <div className= "header-top">
-                <h1> Guildhouse    
-                    <NavLink to="/" className="changelog-Btn">View Changelog</NavLink>
+                <Logo className="logo" src={logo} alt="Guildhouse">     
                     
-                </h1>
-
+                    
+                </Logo>
+                <NavLink to="/" className="changelog-Btn">VIEW CHANGELOG</NavLink>
                 {/* img src={logo} */}
                 {/* logo could go here( svg file) */}
+           </div>
+           {/* header content */}
+           <div className= "header-content">
+                <Link className="compendium">COMPENDIUM</Link>
+                <Link className="hero-Roster">HERO ROSTER</Link>
            </div>
         </HeaderComponent>
     );
   }
 }
 export default Header;
+
+// Logo
+const Logo= styled.img`
+    width:20em;
+    height:5rem;
+    position:absolute;
+    top:25%;
+    left:50%;
+    transform: translate(-50%,-50%);
+`;
 
 //Header Container
 const HeaderComponent = styled.header`
@@ -31,13 +47,51 @@ const HeaderComponent = styled.header`
     line-height: normal;
     border-radius: 0.1875rem;
     font-size: 1rem;
-    background: var(--main-red);
+    background: var(--main-yellow);
     position: absolute;
     translate: transform(-50%, -50%);
     cursor: pointer;
     transition: background 0.2s ease-in; 
     &:hover {
-        background: var(--main-red-hover);
+        background: var(--main-yellow-hover);
       }
+
     }
+    //Header top
+    .header-top{
+        position:relative;
+        height:9rem;
+        z-index:1;
+    }
+    // header content
+    .header-content{
+        width: 65%;
+        position:relative;
+        margin: 4.5rem auto 0;
+        display:flex;
+        justify-content:center;
+        align-content: center;
+        text-align:center;
+        flex-direction:column;
+        z-index:1;
+    }
+    //content buttons
+    .compendium,.hero-Roster{
+        display: inline-block;
+        background: var(--main-yellow);
+        border:none;
+        outline:none;
+        margin:0.33%;
+        padding:1.5rem;
+        border-radius:0.1875rem;
+        font-size: 2rem;
+        text-align: center;
+        box-shadow: 0 1px 0 rgba(0,0,0,0.56);
+        transition: background 0.2s ease-in;
+        cursor:pointer;
+        &:hover{
+            background:var(--main-yellow-hover);
+        }
+    }
+    
 `;
