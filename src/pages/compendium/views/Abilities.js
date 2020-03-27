3199013837c78c7f7ef5757902146ac0034ae527
= <willyliao0425@gmail.com>
2020-03-27 02:59:24 -0700

    included json in data folder, dynamically creates barebone tablist

--- src/pages/compendium/views/Abilities.js
+++ src/pages/compendium/views/Abilities.js
@@ -2,51 +2,160 @@
 import React, { Component } from 'react';
 import logo from '../../../images/Logo.png'; 
 import styled from "styled-components";
+import { Tab, TabPanel, Tabs, TabList } from "react-web-tabs";
+import HeaderTop from '../components/HeaderTop';
+import "../../../css/Tab.css";
+import classes from '../../../data/classes';
+
+
 
 class Abilities extends Component {
+
+  capitalizeWord(word){
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  };
+
+  createTabLabels(object){
+    const object_keys = Object.keys(object);
+    let tabLabels = [];
+
+    object_keys.forEach(key => {
+      let children = object[key].map(elem => ({label: elem.class}));  
+      tabLabels.push({"label": key, "children": children})
+    });
+
+    return tabLabels
+  };
+  
+  createTabs(tabLabels){
+    let tabs = [];
+    
+    tabLabels.forEach(elem => {
+      tabs.push(<Tab>
+        {this.capitalizeWord(elem.label)}
+        </Tab>);
+
+      if (elem.children){
+        tabs.push(this.createTabs(elem.children))
+      }
+    });
+
+    return tabs
+  };
+
   render() {
+    const tabLabels = this.createTabLabels(classes);
+    const tabs = this.createTabs(tabLabels);
+
     return (
-     
+    <HeaderComponent className="main-narrative-tools-container"> 
+      <div className="header-top">
+        <HeaderTop/>
+      </div>
 
-    <div className="main-abilities-container"> 
-        <div className="header-top">
-            <img  alt="Guildhouse" className="Logo" />
-            <h1>fffffffffff</h1>
-        </div>
-
-    </div>
+      <Content>
+        <Tabs vertical>
+          <TabList>
+            {tabs}
+          </TabList>
+        </Tabs>
+      </Content>
+    
+    </HeaderComponent>
     );
   }
 }
 export default Abilities; 
 //Header Container
 const HeaderComponent = styled.header`
-    .changelog-Btn {
-    right: 0;
-    margin: 1.125rem 3% 0;
-    padding: 0.4375rem 1.0625rem;
-    font-wight: 400;
-    line-height: normal;
-    border-radius: 0.1875rem;
-    font-size: 1rem;
-    background: var(--main-yellow);
-    position: absolute;
-    translate: transform(-50%, -50%);
-    cursor: pointer;
-    transition: background 0.2s ease-in;
-    font-family: 'Helvetica Bold', sans-serif;
-    font-weight: 700; 
-    &:hover {
-        background: var(--main-yellow-hover);
+  .changelog-Btn {
+  right: 0;
+  margin: 1.125rem 3% 0;
+  padding: 0.4375rem 1.0625rem;
+  font-wight: 400;
+  line-height: normal;
+  border-radius: 0.1875rem;
+  font-size: 1rem;
+  background: var(--main-yellow);
+  position: absolute;
+  translate: transform(-50%, -50%);
+  cursor: pointer;
+  transition: background 0.2s ease-in;
+  font-family: 'Helvetica Bold', sans-serif;
+  font-weight: 700; 
+  &:hover {
+      background: var(--main-yellow-hover);
+    }
+
+  }
+  //Header top
+  .header-top{
+      position:relative;
+      height:9rem;
+      z-index:1;
+  }
+  // header content
+  .header-content{
+      width: 65%;
+      position:relative;
+      margin: 4.5rem auto 0;
+      display:flex;
+      justify-content:center;
+      align-content: center;
+      text-align:center;
+      flex-direction:column;
+      z-index:1;
+  }
+  //content buttons
+  .compendium,.hero-Roster{
+      display: inline-block;
+      font-family: 'Helvetica Bold', sans-serif;
+      font-weight: 700;
+      background: var(--main-yellow);
+      border:none;
+      outline:none;
+      margin:0.33%;
+      padding:1.5rem;
+      border-radius:0.1875rem;
+      font-size: 2rem;
+      text-align: center;
+      box-shadow: 0 1px 0 rgba(0,0,0,0.56);
+      transition: background 0.2s ease-in;
+      
+      cursor:pointer;
+      &:hover{
+          background:var(--main-yellow-hover);
+          text-decoration: underline;
       }
+  }
+  
+`;
+
+//content
+const Content = styled.body`
+    .narrative-header{
+        top:8%;
+        left:50%;
+        position:absolute;
+        text-align:center;
+        transform: translate(-50%,-5%);
 
     }
-    //Header top
-    .header-top{
-        position:relative;
-        height:9rem;
-        z-index:1;
+    .vertical-tabs{
+        top:15%;
+        position: absolute;
+        
     }
+   
+   
+
+    button:hover{
+        cursor:pointer;
+        background: var(--main-yellow);
+   
+        
+    }
+
     // header content
     .header-content{
         width: 65%;
@@ -60,7 +169,7 @@ const HeaderComponent = styled.header`
         z-index:1;
     }
     //content buttons
-    .compendium,.hero-Roster{
+    .abilities,.how-to-play{
         display: inline-block;
         font-family: 'Helvetica Bold', sans-serif;
         font-weight: 700;
