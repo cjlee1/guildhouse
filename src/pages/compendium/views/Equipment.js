b073152e455204a77e7d4290a838dc19217a9a82
cjlee1 <cjlee@ualberta.ca>
2020-06-04 23:30:57 -0600

    fixed placement of logo

--- src/pages/compendium/views/Equipment.js
+++ src/pages/compendium/views/Equipment.js
@@ -121,36 +121,72 @@ class Equipment extends Component {
   }
 }
 export default Equipment; 
-//Logo
-const Logo= styled.img`
-    // width:20em; if logo is not there
-    height:5rem;
-    position:absolute;
-    left:50%;
-    font-family: 'Helvetica Bold', sans-serif;
-    font-size: 3rem;
-    font-weight: 900;
-    transform: translate(-50%,-5%);
-`;
+
 //Header Container
 const HeaderComponent = styled.header`
+  .changelog-Btn {
+  right: 0;
+  margin: 1.125rem 3% 0;
+  padding: 0.4375rem 1.0625rem;
+  font-wight: 400;
+  line-height: normal;
+  border-radius: 0.1875rem;
+  font-size: 1rem;
+  background: var(--main-yellow);
+  position: absolute;
+  translate: transform(-50%, -50%);
+  cursor: pointer;
+  transition: background 0.2s ease-in;
+  font-family: 'Helvetica Bold', sans-serif;
+  font-weight: 700; 
+  &:hover {
+      background: var(--main-yellow-hover);
+    }
 
-    // header content
-    .header-content{
-        width: 65%;
-        position:relative;
-        margin: 4.5rem auto 0;
-        display:flex;
-        justify-content:center;
-        align-content: center;
-        text-align:center;
-        flex-direction:column;
-        z-index:1;
-    }
-    
-    }
-    
+  }
+  //Header top
+  .header-top{
+      position:relative;
+      height:9rem;
+      z-index:1;
+  }
+  // header content
+  .header-content{
+      width: 65%;
+      position:relative;
+      margin: 4.5rem auto 0;
+      display:flex;
+      justify-content:center;
+      align-content: center;
+      text-align:center;
+      flex-direction:column;
+      z-index:1;
+  }
+  //content buttons
+  .compendium,.hero-Roster{
+      display: inline-block;
+      font-family: 'Helvetica Bold', sans-serif;
+      font-weight: 700;
+      background: var(--main-yellow);
+      border:none;
+      outline:none;
+      margin:0.33%;
+      padding:1.5rem;
+      border-radius:0.1875rem;
+      font-size: 2rem;
+      text-align: center;
+      box-shadow: 0 1px 0 rgba(0,0,0,0.56);
+      transition: background 0.2s ease-in;
+      
+      cursor:pointer;
+      &:hover{
+          background:var(--main-yellow-hover);
+          text-decoration: underline;
+      }
+  }
+  
 `;
+
 //content
 const Content = styled.body`
     .narrative-header{
